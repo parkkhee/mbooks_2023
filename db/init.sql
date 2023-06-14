17402fd33f35040190c724fe39e6b7139cd07781
parkkhee <khp963@naver.com>
2023-06-14 10:50:24 +0900

    mbooks 세ì´초기 세팅

--- db/init.sql
+++ db/init.sql
@@ -1,5 +1,17 @@
-# 개발 DB 생성
-DROP DATABASE IF EXISTS mbooks;
-CREATE DATABASE mbooks;
-USE mbooks;
+#
+개발 DB 생성
+DROP
+DATABASE IF EXISTS mbooks__dev;
+CREATE
+DATABASE mbooks__dev;
+USE
+mbooks__dev;
 
+#
+테스트 DB 생성
+DROP
+DATABASE IF EXISTS mbooks__test;
+CREATE
+DATABASE mbooks__test;
+USE
+mbooks__test;
